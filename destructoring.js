8eff28c9ca64ad650e056a2ce28448a5d17c995a
Dario Elguero <darioelguero@gmail.com>
2021-07-23 03:11:17 -0300

    destructuring listo

--- destructoring.js
+++ destructoring.js
@@ -7,13 +7,15 @@ const original = {
 let sinB = null;
 let valorDeC = null;
 
-/*
-    Utilizando SOLAMENTE destructoring, obtener los siguientes resultados:
-    - sinB: Una copia del objeto original sin la clave "b"
-    - valorDeC: Obtener el valor de la clave "c". (¡No puede hacer "valorDeC = original.c"!)
+    // Utilizando SOLAMENTE destructoring, obtener los siguientes resultados:
+    // - sinB: Una copia del objeto original sin la clave "b"
+    let {a,c} = original
+    sinB = {a,c}
+    // - valorDeC: Obtener el valor de la clave "c". (¡No puede hacer "valorDeC = original.c"!)
+    let {c:valor} = original
+    valorDeC = valor
+    // Reemplazar este comentario con su codigo.
 
-    Reemplazar este comentario con su codigo.
- */
 
 console.log(sinB);
 console.log(valorDeC);
